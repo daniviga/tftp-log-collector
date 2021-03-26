703c866805caca566aeebfc616bf81165ebaf183
Daniele Viganò <daniele@vigano.me>
2021-03-26 19:15:30 +0100

    Fix a typo

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='tftp-log-collector',
     version='0.1.0',
     python_requires='>=3.5',
-    install_requires=['ptfpd'],
+    install_requires=['ptftpd'],
     scripts=['log_collector.py'],
 )
